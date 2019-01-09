7b5cd6c9f0071ddcc93bd0f2cafb87cad776bbf6
hiddendeerer <hiddendeerer@gmail.com>
2019-01-09 18:23:03 +0800

    样式修饰

--- src/home/components/Header.vue
+++ src/home/components/Header.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="header">
     <div class="header-left">返回</div>
-    <div class="header-input">搜索</div>
-    <div class="header-right">图标</div>
+    <div class="header-input">输入城市/景点/游玩主题</div>
+    <div class="header-right">城市</div>
   </div>
 </template>
 
@@ -17,7 +17,7 @@ export default {
   display: flex;
   line-height: 0.86rem;
   background: #00bcd4;
-  color: #fff
+  color: #fff;
 }
 
 .header-left {
@@ -27,11 +27,19 @@ export default {
 
 .header-input {
   flex: 1;
+  height: 0.64rem;
+  line-height: 0.64rem;
+  margin-top: 0.12rem;
+  margin-left: 0.2rem;
+  background: #fff;
+  border-radius: 0.1rem;
+  color: #ccc;
 }
 
 .header-right {
   width: 1.24rem;
   float: right;
+  text-align: center;
 }
 </style>
 
