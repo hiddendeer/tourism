2d6452b35b098ff6311fca6ed881e6f120135623
hiddendeer <hiddendeerer@gmail.com>
2019-01-10 23:13:59 +0800

    引入阿里图标和优化CSS文件

--- src/home/components/Header.vue
+++ src/home/components/Header.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="header">
-    <div class="header-left">返回</div>
-    <div class="header-input">输入城市/景点/游玩主题</div>
-    <div class="header-right">城市</div>
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe64a;</span>
+    </div>
   </div>
 </template>
 
@@ -13,10 +21,12 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+
 .header {
   display: flex;
   line-height: 0.86rem;
-  background: #00bcd4;
+  background: $bgColor;
   color: #fff;
 }
 
@@ -25,12 +35,18 @@ export default {
   float: left;
 }
 
+.back-icon {
+  text-align: center;
+  font-size: 0.4rem;
+}
+
 .header-input {
   flex: 1;
   height: 0.64rem;
   line-height: 0.64rem;
   margin-top: 0.12rem;
   margin-left: 0.2rem;
+  padding-left: 0.2rem;
   background: #fff;
   border-radius: 0.1rem;
   color: #ccc;
@@ -40,6 +56,11 @@ export default {
   width: 1.24rem;
   float: right;
   text-align: center;
+}
+
+.arrow-icon {
+  margin-left: -0.04rem;
+  font-size: 0.24rem;
 }
 </style>
 
--- src/main.js
+++ src/main.js
@@ -1,18 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import './assets/styles/reset.css'
-import './assets/styles/border.css'
-import fastClick from 'fastclick'
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
+import "styles/reset.css";
+import "styles/border.css";
+import "styles/iconfont.css";
+import fastClick from "fastclick";
 
-Vue.config.productionTip = false
-fastClick.attach(document.body)
+Vue.config.productionTip = false;
+fastClick.attach(document.body);
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   components: { App },
-  template: '<App/>'
-})
+  template: "<App/>"
+});
