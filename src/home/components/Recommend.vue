3244f4caa50a8fbd1de1cff10d86b41197147a81
hiddendeerer <hiddendeerer@gmail.com>
2019-01-14 11:39:00 +0800

    推荐热销组件完成

--- src/home/components/Recommend.vue
+++ src/home/components/Recommend.vue
@@ -2,15 +2,15 @@
   <div>
     <div class="title">热销推荐</div>
     <ul>
-      <li class="item border-bottom">
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
         <img
           class="item-img"
-          src="http://img1.qunarzz.com/sight/p0/1508/f7/f7c3be996152011c.img.jpg_200x200_0b707f70.jpg"
+          :src="item.imgUrl"
         >
 
         <div class="item-info">
-          <p class="item-title">南京总统府</p>
-          <p class="item-desc">南京总统府现在已成为记录中国近代史的博物馆。</p>
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
           <button class="item-button">查看详情</button>
         </div>
       </li>
@@ -20,7 +20,37 @@
 
 <script>
 export default {
-  name: "HomeRecommend"
+  name: "HomeRecommend",
+  data () {
+    return {
+      recommendList:[
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1508/f7/f7c3be996152011c.img.jpg_200x200_0b707f70.jpg',
+          title: '南京总统府',
+          desc: '南京总统府现在已成为记录中国近代史的博物馆.'
+        },
+              {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1508/f7/f7c3be996152011c.img.jpg_200x200_0b707f70.jpg',
+          title: '南京总统府',
+          desc: '南京总统府现在已成为记录中国近代史的博物馆。'
+        },
+              {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1508/f7/f7c3be996152011c.img.jpg_200x200_0b707f70.jpg',
+          title: '南京总统府',
+          desc: '南京总统府现在已成为记录中国近代史的博物馆。'
+        },
+              {
+          id: '0004',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1508/f7/f7c3be996152011c.img.jpg_200x200_0b707f70.jpg',
+          title: '南京总统府',
+          desc: '南京总统府现在已成为记录中国近代史的博物馆。'
+        },
+      ]
+    }
+  }
 };
 </script>
 
