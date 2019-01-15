551361792e5878da5f124e41b2f0e5fdad6e1f88
hiddendeerer <hiddendeerer@gmail.com>
2019-01-15 14:21:23 +0800

    城市页顶部

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,17 +1,21 @@
 <template>
+  <div>
     <city-header></city-header>
+    <city-search></city-search>
+  </div>
 </template>
 
 <script>
-import CityHeader from './components/Header'
+import CityHeader from "./components/Header";
+import CitySearch from "./components/Search";
 export default {
-    name:"City",
-    components:{
-        CityHeader
-    }
-}
+  name: "City",
+  components: {
+    CityHeader,
+    CitySearch
+  }
+};
 </script>
 
 <style lang="stylus" scoped>
-
 </style>
