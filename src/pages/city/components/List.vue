088e48e0a45b5d8b69ceafc267f760f51c53c306
hiddendeerer <hiddendeerer@gmail.com>
2019-01-21 18:12:58 +0800

    搜索页列表

---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="list">
+      <div class="area">
+          <div class="title">当前城市</div>
+      </div>
+        <div class="area">
+          <div class="title">热门城市</div>
+      </div>
+        <div class="area">
+          <div class="title">A</div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CityList"
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+</style>
