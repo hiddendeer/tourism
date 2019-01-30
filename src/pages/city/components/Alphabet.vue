505f687dec1b68df353b216ea4f797b11c687a91
hiddendeerer <hiddendeerer@gmail.com>
2019-01-30 14:45:27 +0800

    新增右边栏

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,13 +1,11 @@
 <template>
-  <div class="list">
-      <ul>
-          <li>A</li>
-          <li>A</li>
-          <li>A</li>
-          <li>A</li>
-          <li>A</li>
-      </ul>
-  </div>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </ul>
 </template>
 
 <script>
@@ -19,4 +17,20 @@ export default {
 <style lang="stylus" scoped>
 @import '~styles/varibles.styl';
 
+.list {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: absolute;
+  top: 1.58rem;
+  right: 0;
+  bottom: 0;
+  width: 0.4rem;
+}
+
+.item {
+  line-height: 0.44rem;
+  text-align: center;
+  color: $bgColor;
+}
 </style>
