3c828d24d25f8a6fd3564a728b1ba1c5f159e287
hiddendeer <hiddendeerer@gmail.com>
2019-02-20 22:02:12 +0800

    优化联动

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -27,8 +27,13 @@ export default {
   },
   data() {
     return {
-      touchStatus: false
+      touchStatus: false,
+      startY: 0,
+      timer: null
     };
+  },
+  updated () {
+    this.startY = this.$refs["A"][0].offsetTop;
   },
   methods: {
     handleLetterClick(e) {
@@ -39,12 +44,16 @@ export default {
     },
     handleTouchMove(e) {
       if (this.touchStatus) {
-        const startY = this.$refs["A"][0].offsetTop;
-        const touchY = e.touches[0].clientY - 79;
-        const index = Math.floor((touchY - startY) / 20); 
-        if (index >= 0 && index < this.letters.length) {
-          this.$emit('change', this.letters[index]);
+        if (this.timer) {
+          clearTimeout(this.timer);
         }
+        this.timer = setTimeout(() => {
+          const touchY = e.touches[0].clientY - 79;
+          const index = Math.floor((touchY - this.startY) / 20); 
+          if (index >= 0 && index < this.letters.length) {
+            this.$emit('change', this.letters[index]);
+          }
+        },16);
       }
     },
     handleTouchEnd() {
